b77d5bd6b5c70c8ec2da871c003ffa00e38c092c
zdwolfe <wolfe.zach@gmail.com>
2013-11-04 17:49:13 -0500

    Now we can set mongodb variable from environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,14 @@
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-nodemon');
+  console.log(JSON.stringify(process.env));
 
   grunt.initConfig({
     nodemon: {
       dev: {
         options: {
           env: {
-            'LIBRECMS_MONGO_URI': 'mongodb://localhost/librecms',
+            'LIBRECMS_MONGO_URI': process.env.LIBRECMS_MONGO_URI || 'mongodb://localhost/librecms',
             'LIBRECMS_API_PORT': '3030'
           }
         }
