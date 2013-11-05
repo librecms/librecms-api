3abe70991c1cc78c783329de1cc014dd4a624e00
zdwolfe <wolfe.zach@gmail.com>
2013-11-04 23:11:02 -0500

    Removing CSRF un-protection

--- api.js
+++ api.js
@@ -10,20 +10,6 @@ app.configure(function () {
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-  };
-  app.use(allowCrossDomain);
 });
 
 var routes = require('./routes');
