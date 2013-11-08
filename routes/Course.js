d8b91c439dea374a8485513e9ff6d3eb266dde3c
zdwolfe <wolfe.zach@gmail.com>
2013-11-08 16:57:02 -0500

    Not using aggregation framework

--- routes/Course.js
+++ routes/Course.js
@@ -3,7 +3,6 @@ var Course = mongoose.model('Course');
 var Event = mongoose.model('Event');
 var Post = mongoose.model('Post');
 var User = mongoose.model('User');
-var Assignment = mongoose.model('Assignment');
 
 var CourseCtrl = {
   init: function(app) {
@@ -31,19 +30,15 @@ var CourseCtrl = {
 
     // Get course by id
     app.get('/courses/:courseId', function(req, res, next) {
-      var pipe = [
-        {
-          $match: { _id: mongoose.Types.ObjectId(req.params.courseId) },
-          $unwind: "$posts",
-          $group: { _id: "$_id", posts: { $push: "$posts" } }
-        }
-      ];
-      Course.aggregate(pipe, function(err, result) {
-        if (err) return next(err);
-        if (!result) return next(null, false);
-        console.dir(result);
-        return res.json(result);
-      });
+      var query = {
+        _id: req.params.courseId
+      };
+      Course.findOne(query)
+        .exec(function(err, course) {
+          if (err) return next(err);
+          if (!course) return res.end(404);
+          return res.json(course);
+        });
     });
 
     // Register a User to a Course (add user to 'students' set)
@@ -74,8 +69,7 @@ var CourseCtrl = {
       var options = { "new": true };
       Course.findOneAndUpdate(query, update, options)
         .exec(function(err, course) {
-          if (err) return next(err);
-          if (!course) return next(null, false);
+          if (err) throw err;
           return res.json(201, newPost);
         });
     });
@@ -88,7 +82,7 @@ var CourseCtrl = {
       var query = { _id: req.params.courseId };
       Course.findOne(query, filter)
         .exec(function(err, course) {
-          if (err) return next(err);
+          if (err) throw err;
           if (!course) return res.end(404);
           return res.json(course.posts || []);
         });
@@ -108,8 +102,7 @@ var CourseCtrl = {
       var query = { _id: req.params.courseId };
       Course.findOneAndUpdate(query, update, options)
         .exec(function(err, course) {
-          if (err) return next(err);
-          if (!course) return next(null, false);
+          if (err) throw err;
           return res.json(201, newEvent);
         });
     });
@@ -122,7 +115,7 @@ var CourseCtrl = {
       var query = { _id: req.params.courseId };
       Course.findOne(query, filter)
         .exec(function(err, course) {
-          if (err) return next(err);
+          if (err) throw err;
           if (!course) return res.end(404);
           return res.json(course.events || []);
         });
@@ -131,32 +124,14 @@ var CourseCtrl = {
     // Get assignments by course ID
     // @TODO pagination
     app.get('/courses/:courseId/assignments', function(req, res, next) {
-      var filter = { assignments: true };
+      var filter = { assignment: true };
       var startDate = req.body.startDate || (new Date()).getTime();
       var query = { _id: req.params.courseId };
       Course.findOne(query, filter)
         .exec(function(err, course) {
-          if (err) return next(err);
+          if (err) throw err;
           if (!course) return res.end(404);
           return res.json(course.assignments || []);
-        });
-    });
-
-    // Make a new Assignment by course ID
-    app.post('/courses/:courseId/assignments', function(req, res, next) {
-      var newAssignment = new Assignment({
-        due: req.body.due,
-        description: req.body.description,
-        posted: (new Date()).getTime()
-      });
-      var update = { $push: { assignments: newAssignment } };
-      var options = { "new": true };
-      var query = { _id: req.params.courseId };
-      Course.findOneAndUpdate(query, update, options)
-        .exec(function(err, course) {
-          if (err) return next(err);
-          if (!course) return next(null, false);
-          return res.json(201, newAssignment);
         });
     });
 
