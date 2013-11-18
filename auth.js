48efb73e3270037dcd45d58e9be502f0a68c333e
zdwolfe <wolfe.zach@gmail.com>
2013-11-18 18:21:02 -0500

    Authentication on DELETE assignment by assignment faililng to recognize authenticated user

--- auth.js
+++ auth.js
@@ -29,6 +29,25 @@ passport.use(new LocalStrategy(function(username, password, next) {
 }));
 
 module.exports.ensureAuthenticated = function(req, res, next) {
+  console.log('ensureAuthenticated isAuthenticated = ' + req.isAuthenticated());
+  console.log('ensureAuthenticated user = ' + JSON.stringify(req.user, null, 4));
   if (req.isAuthenticated()) { return next(); }
   res.status(401).end();
+};
+
+module.exports.ensureInstructor = function(req, res, next) {
+  console.log('ensureInstructor req.user = ' + JSON.stringify(req.user, null, 4));
+  if (!req.isAuthenticated() || !req.user || !req.user.role ||
+      req.user.role !== 'instructor') {
+    return res.status(403).end();
+  }
+  return next();
+};
+
+module.exports.ensureStudent = function(req, res, next) {
+  if (!req.isAuthenticated() || !req.user || !req.user.role ||
+      req.user.role !== 'student') {
+    return res.status(401).end();
+  }
+  return next();
 };
--- routes/Assignment.js
+++ routes/Assignment.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
-var Submission = mongoose.model('AssignmentSubmission');
 var auth = require('../auth');
+
+var Submission = mongoose.model('AssignmentSubmission');
+var Course = mongoose.model('Course');
 
 var AssignmentCtrl = {
   init: function(app) {
@@ -32,6 +34,37 @@ var AssignmentCtrl = {
         newSubmission.save(function(err) {
           if (err) return next(err);
           return res.json(newSubmission);
+        });
+      });
+
+    app.del('/courses/:courseId/assignments/:assignmentId',
+      auth.ensureAuthenticated,
+      auth.ensureInstructor,
+      function(req, res, next) {
+        console.log('delete');
+        req.assert('courseId').is(/^[0-9a-fA-F]{24}$/);
+        req.assert('assignmentId').is(/^[0-9a-fA-F]{24}$/);
+        var errors = req.validationErrors();
+        if (errors) {
+          return res.send('There have been validation errors: ' + util.inspect(errors), 400);
+        }
+
+        // Find course by this course/assignmetn id tuple
+        var query = { 
+          _id: req.params.courseId,
+          "assignments._id": req.params.assignmentId,
+          instructors: req.user._id
+        };
+        // Pull the assignment with this id from the assignments list.
+        var update = {
+          $pull: { assignments: { _id: req.params.assignmentId } }
+        };
+        var options = { 'new': true };
+        Course.findOneAndUpdate(query, update, options)
+          .exec(function(err, course) {
+            if (err) return next(err);
+            if (!course) return next(null, false);
+            return res.status(200).end();
         });
       });
   }
